b44c77ec3a13bcd10fb455bf686c804eebc8025a
Toni Guijarro <aguijarror@esliceu.net>
2022-01-31 20:17:11 +0100

    SIngle y footer

---
+++ footer.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar-light">
+      <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+          <?php
+          wp_nav_menu(
+            array(
+              'theme_location'  => 'temazo_menu_principal',
+              'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
+              'container'       => 'div',
+              'container_class' => 'collapse navbar-collapse',
+              'container_id'    => 'navbarNav',
+              'menu_class'      => 'navbar-nav mr-auto',
+              'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+              'walker'          => new WP_Bootstrap_Navwalker(),
+            )
+          );
+          ?>
+      </div>
+    </nav>
+
